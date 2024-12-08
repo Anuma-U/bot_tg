8da316f3ccb12cc62ebe40fda91e94120f863c99
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-12-08 20:49:16 +0300

    module_13_6.py

--- module_13_6.py
+++ module_13_6.py
@@ -1,5 +1,3 @@
-from gc import callbacks
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
