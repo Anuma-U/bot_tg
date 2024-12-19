94b04ad038344262b4fc5593c11b3f273ebfd0c8
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-12-19 08:52:51 +0300

    Add files via upload

--- module_14_4.py
+++ module_14_4.py
@@ -5,7 +5,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import crud_functions
 
-api = "7798422390:AAGiBWXkoTHYMOgiBTfKgMWePVO9ia0tQKc"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
